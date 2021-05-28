b4fe39e3a0c40ca97a254b1190649c8b6261796a
saijyothinuthakki <74003554+saijyothinuthakki@users.noreply.github.com>
2021-05-28 21:32:20 +0530

    Update App.js

--- Part1/ex-1.6/src/App.js
+++ Part1/ex-1.6/src/App.js
@@ -3,16 +3,16 @@ import React, {useState} from 'react';
 const Statistics = ({good ,bad,neutral}) => {
     return (
         <div>
-           <h4><Statistic value={good} text="good"/></h4>         
-            <h4><Statistic value={neutral} text="neutral"/></h4>                   
-            <h4><Statistic  value={bad} text="bad"/></h4>             
+           <h4><Stat value={good} text="good"/></h4>         
+            <h4><Stat value={neutral} text="neutral"/></h4>                   
+            <h4><Stat value={bad} text="bad"/></h4>             
        </div>
     )
 }
 
-const Button = ({handleClick,text}) => {
+const Button = ({ClickOn,text}) => {
     return (
-        <button onClick={handleClick}>{text}</button>
+        <button onClick={ClickOn}>{text}</button>
     )
 }
 const App = () => {
@@ -22,16 +22,16 @@ const App = () => {
     return (
         <div>
             <h1>Hello Customers Please Give Your Feedback </h1>
-                <Button handleClick={()=> setGood(good+1)} text="Good" />
-                <Button handleClick={()=> setNeutral(neutral+1)} text="Neutral" />
-                <Button handleClick={()=> setBad(bad+1)} text="Bad" />           
+                <Button ClickOn={()=> setGood(good+1)} text="Good" />
+                <Button ClickOn={()=> setNeutral(neutral+1)} text="Neutral" />
+                <Button ClickOn={()=> setBad(bad+1)} text="Bad" />           
             <h2>statistics</h2>
             <Statistics good={good} bad ={bad} neutral={neutral} />
         </div>
     )
 }
 
-const Statistic = ({text , value}) => {
+const Stat = ({text , value}) => {
   return (
       <h4>{text} {value}</h4>
   )
