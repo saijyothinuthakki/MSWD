52f834b9c008dc053a0ee8e1b25aa4cfbb0d9cfa
saijyothinuthakki <74003554+saijyothinuthakki@users.noreply.github.com>
2021-06-09 23:09:47 +0530

    Add files via upload

---
+++ Part2/phonebook/src/services/persons.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+const baseUrl = '/api/persons';
+
+const getAll = () => (
+    axios
+        .get(baseUrl)
+        .then(response => response.data)
+);
+const create = (name, number) => (
+    axios
+        .post(baseUrl, {name, number})
+        .then(response => response.data)
+);
+const pop = id => axios.delete(`${baseUrl}/${id}`);
+const update = (id, updatedPerson) => (
+    axios
+        .put(`${baseUrl}/${id}`, updatedPerson)
+        .then(response => response.data)
+);
+
+export default {getAll, create, pop, update};
